9007711ed31f429f2a876a12b816c7be930e7d27
Deniz <deniztoma512@gmail.com>
2019-10-24 17:14:17 +0200

    user_stats wip

--- src/gitgetter/api/__init__.py
+++ src/gitgetter/api/__init__.py
@@ -1,7 +1,8 @@
-from .api import user_active, repo_downwards, repo_downwards1
+from .api import user_active, repo_downwards, repo_downwards1, user_stats
 
 __all__ = [
     'user_active',
+    'user_stats',
     'repo_downwards',
     'repo_downwards1',
 ]
